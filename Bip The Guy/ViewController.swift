f23064394224084bd1d7790cf4f55345939c1d83
Anastasia Kyratzoglou <kyratzoa@bc.edu>
2019-09-18 11:13:26 -0400

    4.4 Completed - Brief Intro to Classes, Protocols, and Delegates as We Learn to Use UIImagePickerController to Access the Photo Library and Camera

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: properties
     @IBOutlet weak var imageToPunch: UIImageView!
+    
     var audioPlayer = AVAudioPlayer()
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,18 @@ class ViewController: UIViewController {
         
         UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageToPunch.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: false, completion: nil)
+    }
+    
     func playSound(soundName: String, audioPlayer: inout AVAudioPlayer){
         //Can we load in the file soundName
         if let sound = NSDataAsset(name: soundName){
@@ -45,11 +59,30 @@ class ViewController: UIViewController {
         }
     }
     
+    func showAlert(title: String, message:String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     //MARK: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     @IBAction func cameraPressed(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = .camera
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            showAlert(title: "Camera not Available", message: "There is no camera available on this device.")
+        }
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
